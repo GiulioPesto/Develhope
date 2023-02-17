3c2ebcd991f22385cb5185fdde7b9060daf45534
pesto <giulio.pastore2410@gmail.com>
2023-02-17 20:09:37 +0100

    exercise_17

---
+++ src/it/develhope/JavaOOP/exercise_1/Course.java
@@ -0,0 +1,16 @@
+package it.develhope.JavaOOP.exercise_1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Course {
+    ArrayList<Student> students = new ArrayList<>();
+    String name;
+    int age;
+    String courseName;
+    int maxStudents;
+    byte qualityRatingOutOf10;
+    byte mediumStudentsAge;
+    byte monthsForCourse;
+}
---
+++ src/it/develhope/JavaOOP/exercise_1/Student.java
@@ -0,0 +1,6 @@
+package it.develhope.JavaOOP.exercise_1;
+
+public class Student {
+    String name;
+    int age;
+}
